dff22b002afb0c066ff6f0b2b6b27eab7820a574
jjzmajic <uros.m.perisic@gmail.com>
2019-03-31 01:18:27 -0700

    Fix another dumbfuck wal-theme.el mistake referring to GUI.

--- wal-theme.el
+++ wal-theme.el
@@ -3,6 +3,6 @@
 (deftheme wal-theme-gui "Wal theme, the version that picks
 beetween GUI and TTY colors using `display-graphic-p'")
 
-(wal-theme-create-theme 'wal-theme-gui)
+(wal-theme-create-theme 'wal-theme)
 
 (provide-theme 'wal-theme)
