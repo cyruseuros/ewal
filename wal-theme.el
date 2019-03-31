016647b00e244088bd1bdbe2613cc0a38749e890
jjzmajic <uros.m.perisic@gmail.com>
2019-03-30 21:56:53 -0700

    Add autodetect version of wal-theme.

---
+++ wal-theme.el
@@ -0,0 +1,8 @@
+(require 'wal-theme-common)
+
+(deftheme wal-theme-gui "Wal theme, the version that picks
+beetween GUI and TTY colors using `display-graphic-p'")
+
+(wal-theme-create-theme 'wal-theme-gui)
+
+(provide-theme 'wal-theme-gui)
