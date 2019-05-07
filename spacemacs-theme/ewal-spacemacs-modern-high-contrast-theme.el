8a5188aba5e1e6db556a844eec86059976d700fe
Uros Perisic <uros.m.perisic@gmail.com>
2019-05-07 05:30:35 -0400

    Fix modern theme customizations.

--- spacemacs-theme/ewal-spacemacs-modern-high-contrast-theme.el
+++ spacemacs-theme/ewal-spacemacs-modern-high-contrast-theme.el
@@ -5,14 +5,14 @@
 
 (deftheme ewal-spacemacs-modern-high-contrast)
 
+;; must be run before `create-spacemacs-theme'
+(custom-theme-set-faces
+ 'ewal-spacemacs-modern-high-contrast
+ (let ((class '((class color) (min-colors 89))))
+   `(line-number ((,class
+                   (:foreground ,(alist-get 'lnum spacemacs-theme-custom-colors)
+                    :background ,(alist-get 'bg1 spacemacs-theme-custom-colors)
+                    :inherit default))))))
 (create-spacemacs-theme 'dark 'ewal-spacemacs-modern-high-contrast)
-(with-eval-after-load 'ewal-spacemacs-modern-high-contrast-theme
-  (custom-theme-set-faces
-   'ewal-spacemacs-modern-high-contrast
-   (let ((class '((class color) (min-colors 89))))
-     `(line-number
-       ((,class (:foreground ,(alist-get 'lnum spacemacs-theme-custom-colors)
-                 :background ,(alist-get 'bg1 spacemacs-theme-custom-colors)
-                 :inherit default)))))))
 
 (provide-theme 'ewal-spacemacs-modern-high-contrast)
--- spacemacs-theme/ewal-spacemacs-modern-theme.el
+++ spacemacs-theme/ewal-spacemacs-modern-theme.el
@@ -1,18 +1,18 @@
 (require 'ewal-spacemacs)
-;; has to be run before loading spacemacs-common
+;; has to be run before loading `spacemacs-common'
 (ewal-spacemacs-get-colors :apply t)
 (require 'spacemacs-common)
 
 (deftheme ewal-spacemacs-modern)
 
+;; must be run before `create-spacemacs-theme'
+(custom-theme-set-faces
+ 'ewal-spacemacs-modern
+ (let ((class '((class color) (min-colors 89))))
+   `(line-number ((,class
+                   (:foreground ,(alist-get 'lnum spacemacs-theme-custom-colors)
+                    :background ,(alist-get 'bg1 spacemacs-theme-custom-colors)
+                    :inherit default))))))
 (create-spacemacs-theme 'dark 'ewal-spacemacs-modern)
-(with-eval-after-load 'ewal-spacemacs-modern-theme
-  (custom-theme-set-faces
-   'ewal-spacemacs-modern
-   (let ((class '((class color) (min-colors 89))))
-     `(line-number
-       ((,class (:foreground ,(alist-get 'lnum spacemacs-theme-custom-colors)
-                 :background ,(alist-get 'bg1 spacemacs-theme-custom-colors)
-                 :inherit default)))))))
 
 (provide-theme 'ewal-spacemacs-modern)
