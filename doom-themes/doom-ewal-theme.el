ce48b256ca5743e0c993f2f5fb1a747cad3b3add
Uros Perisic <uros.m.perisic@gmail.com>
2019-09-06 13:14:15 +0800

    Add directory to custom theme load path.

--- doom-themes/doom-ewal-theme.el
+++ doom-themes/doom-ewal-theme.el
@@ -201,4 +201,10 @@ determine the exact padding."
   ()
   )
 
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'doom-ewal)
 ;;; doom-ewal-theme.el ends here
